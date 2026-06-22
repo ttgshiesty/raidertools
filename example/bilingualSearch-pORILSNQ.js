989e4445e123bd34e6785fee6601b128a2fa7892
🎲ttg-shiesty🎲 <konarrtist@outlook.com>
2026-06-21 20:31:49 -0400

    befor

---
+++ example/bilingualSearch-pORILSNQ.js
@@ -0,0 +1,1 @@
+import{t as e}from"./index-DDJvTLFE.js";function o(o,t,r){if(!t)return!0;const s=t.toLowerCase();return!!o.toLowerCase().includes(s)||!!e(o,"it").toLowerCase().includes(s)}export{o as m};
